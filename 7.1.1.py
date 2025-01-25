8d858c4d3f62aaa408f852808c6e8d54656ab8da
Patryk Suwart <spatryk3123@gmail.com>
2025-01-25 18:12:49 +0100

    Dodałem dwa pierwsze zadania

---
+++ 7.1.1.py
@@ -0,0 +1,5 @@
+# ZADANIE 7.1.1
+# Napisz program drukujący na ekranie 19 gwiazdek:
+# *******************
+
+print('*' * 19)
---
+++ 7.1.2.py
@@ -0,0 +1,10 @@
+# ZADANIE 7.1.2
+# Napisz program drukujący na ekranie wieżę z literek O:
+# O
+# OO
+# OOO
+# OOOO
+# OOOOO
+
+for o in range(6):
+    print('O' * o)
